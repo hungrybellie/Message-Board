dc624c60b8f0f4b524bdcb99a0d3784c3b4a64c5
Bella <63426032+hungrybellie@users.noreply.github.com>
2023-12-06 17:48:49 -0800

    Add files via upload

---
+++ C4Ctechnical/script.js
@@ -0,0 +1,28 @@
+let messages = [];
+
+const textInput = document.getElementById('message');
+const messageArea = document.getElementById('content');
+
+function addMessage() {
+    if (textInput.value === '' || textInput.value.length === 128 ) {
+        alert("Your message must not be empty or over 128 words long.");
+    } else {
+        let messageCard = document.createElement("SPAN");
+        messageCard.innerHTML = textInput.value;
+        messageArea.appendChild(messageCard);
+    }
+    textInput.value = "";
+    saveMessages();
+}
+
+function saveMessages() {
+    localStorage.setItem("persistMessages", messageArea.innerHTML);
+    // localStorage.clear();
+}
+
+ function showMessages() {
+    messageArea.innerHTML = localStorage.getItem("persistMessages");
+ }
+
+ showMessages();
+
